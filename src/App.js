43ae634de6f60ab5b39c0067494447c9d72aab15
Huseynov Turab Taryel <huseynov_t@itstep.edu.az>
2024-07-10 21:58:43 +0400

    useEffect-urok

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Loader } from "./Loader/Loader";
 
-function App() {
+import "./App.scss";
+
+export const App = () => {
+  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  const [selectedCategory, setSelectedCategory] = useState("all");
+
+  useEffect(() => {
+    let url = "https://fakestoreapi.com/products";
+
+    if (selectedCategory !== "all") {
+      url += `/category/${selectedCategory}`;
+    }
+
+    setLoading(true);
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((json) => {
+        setTimeout(() => {
+          setProducts(json);
+          setLoading(false);
+        }, 500);
+      });
+  }, [selectedCategory]);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products/categories")
+      .then((res) => res.json())
+      .then((json) => setCategory(json));
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <select
+        onChange={(e) => {
+          setSelectedCategory(e.target.value);
+        }}
+      >
+        <option key={"all"} value={"all"}>
+          all
+        </option>
+        {category.map((category) => {
+          return (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          );
+        })}
+      </select>
 
-export default App;
+      <div className="product-list">
+        {products.map((item) => {
+          return (
+            <div className="product-item" key={item.id}>
+              <div>{item.category}</div>
+              <div>{item.description}</div>
+              <img
+                style={{ width: "200px", height: "200px" }}
+                src={item.image}
+                alt=""
+              />
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
